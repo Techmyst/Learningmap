cb47a52dd6a52ce3f60467a577b7fee3ba5f9709
Techmyst <117128404+Techmyst@users.noreply.github.com>
2023-04-17 00:20:09 +0530

    Update form.js

--- cse2/form.js
+++ cse2/form.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from "https://www.gstatic.com/firebasejs/9.19.1/firebase/auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyAlhgRL7F4q6XhUl0oYS1OnkIXzIn48guo",
   authDomain: "https://learningmap.netlify.app/",
